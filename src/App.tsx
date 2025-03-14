8555c869b5ff05e7fc7b290dc687da56c636b1ce
Shyam <gshyamprasad2001@gmail.com>
2025-03-14 14:11:35 +0530

    calenderr ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import Calender from "./components/Calender"
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+    <Calender />
     </>
     
   )
