bef7f2bab42d5652ccf073b2588b1a05e882fed9
Shyam <gshyamprasad2001@gmail.com>
2025-03-19 07:39:41 +0530

    FEAT: addes date today and previous months

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -6,6 +6,7 @@ import {
   startOfWeek,
 } from "date-fns";
 import { useMemo, useState } from "react";
+import CalenderDay from "./CalenderDay";
 
 const Calender = () => {
   const [selectedMoth, setSelectedMonth] = useState(new Date());
@@ -27,29 +28,8 @@ const Calender = () => {
         <span className="month-title">June 2023</span>
       </div>
       <div className="days">
-        {calenderDays.map((day) => (
-          <div key={day.getTime()} className="day non-month-day old-month-day">
-            <div className="day-header">
-              <div className="week-name">Sun</div>
-              <div className="day-number">28</div>
-              <button className="add-event-btn">+</button>
-            </div>
-            <div className="events">
-              <button className="all-day-event blue event">
-                <div className="event-name">Short</div>
-              </button>
-              <button className="all-day-event green event">
-                <div className="event-name">
-                  Long Event Name That Just Keeps Going
-                </div>
-              </button>
-              <button className="event">
-                <div className="color-dot blue"></div>
-                <div className="event-time">7am</div>
-                <div className="event-name">Event Name</div>
-              </button>
-            </div>
-          </div>
+        {calenderDays.map((day,index) => (
+         <CalenderDay key={day.getTime()}  day = {day} showWeekName={index <7} selectedMonth={selectedMoth}/>
         ))}
        
       </div>
@@ -58,5 +38,3 @@ const Calender = () => {
 };
 
 export default Calender;
-
-function CalenderDay() {}
---
+++ src/utils/formatDate.tsx
@@ -0,0 +1,5 @@
+
+
+export function formatDate(date: Date, options?: Intl.DateTimeFormatOptions) {
+  return new Intl.DateTimeFormat(undefined, options).format(date);
+}
