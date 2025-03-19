29b8e92d4f85f527254989e8cf628ea7930b7adf
Shyam <gshyamprasad2001@gmail.com>
2025-03-19 07:47:21 +0530

    Added Prev and next Months and default Today Event

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,12 +1,15 @@
 import {
+  addMonths,
   eachDayOfInterval,
   endOfMonth,
   endOfWeek,
   startOfMonth,
   startOfWeek,
+  subMonths,
 } from "date-fns";
 import { useMemo, useState } from "react";
 import CalenderDay from "./CalenderDay";
+import { formatDate } from "../utils/formatDate";
 
 const Calender = () => {
   const [selectedMoth, setSelectedMonth] = useState(new Date());
@@ -20,18 +23,34 @@ const Calender = () => {
   return (
     <div className="calendar">
       <div className="header">
-        <button className="btn">Today</button>
+        <button onClick={() => setSelectedMonth(new Date())} className="btn">
+          Today
+        </button>
         <div>
-          <button className="month-change-btn">&lt;</button>
-          <button className="month-change-btn">&gt;</button>
+          <button
+            onClick={() => setSelectedMonth((m) => subMonths(m, 1))}
+            className="month-change-btn"
+          >
+            &lt;
+          </button>
+          <button
+            onClick={() => setSelectedMonth((m) => addMonths(m, 1))}
+            className="month-change-btn"
+          >
+            &gt;
+          </button>
         </div>
-        <span className="month-title">June 2023</span>
+        <span className="month-title">{formatDate(selectedMoth,{month:"long",year:"numeric"})}</span>
       </div>
       <div className="days">
-        {calenderDays.map((day,index) => (
-         <CalenderDay key={day.getTime()}  day = {day} showWeekName={index <7} selectedMonth={selectedMoth}/>
+        {calenderDays.map((day, index) => (
+          <CalenderDay
+            key={day.getTime()}
+            day={day}
+            showWeekName={index < 7}
+            selectedMonth={selectedMoth}
+          />
         ))}
-       
       </div>
     </div>
   );
